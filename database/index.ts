571b3bfc327b8ae0fe90eeb4e45e99c357e4b3d8
leensungzero <brianwoo119@naver.com>
2019-06-06 22:24:28 +0900

    fixed: Increasing environment variables reusability
    index.ts, s3.tx, passwordEncryption.ts, fbCheckCode.ts
     - ADD: Increase reusability by assigning environment variables to one object

--- database/index.ts
+++ database/index.ts
@@ -4,12 +4,19 @@ import { Sequelize } from 'sequelize-typescript';
 
 dotenv.config();
 
+const DB_CONFIG = {
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+};
+
 export const sequelize = new Sequelize({
-  host: process.env.DB_HOST,
+  host: DB_CONFIG.DB_HOST,
   dialect: 'mysql',
-  database: process.env.DB_NAME,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  database: DB_CONFIG.DB_NAME,
+  username: DB_CONFIG.DB_USERNAME,
+  password: DB_CONFIG.DB_PASSWORD,
   define: {
     charset: 'utf8',
     collate: 'utf8_general_ci',
--- routes/middlewares/user/phone/fbCheckCode.ts
+++ routes/middlewares/user/phone/fbCheckCode.ts
@@ -7,13 +7,19 @@ import CustomError from '@Middleware/error/customError';
 
 dotenv.config();
 
+const FB_CONFIG = {
+  FB_VERSION: process.env.FB_VERSION,
+  FB_APP_ID: process.env.FB_APP_ID,
+  FB_APP_SECRET: process.env.FB_APP_SECRET,
+};
+
 const fbIssueToken = async (req: Request, res: Response, next: NextFunction) => {
-  const fbIssueUrl = `https://graph.accountkit.com/${process.env.FB_VERSION}/access_token?`;
-  const fbVerifyUrl = `https://graph.accountkit.com/${process.env.FB_VERSION}/me?`;
+  const fbIssueUrl = `https://graph.accountkit.com/${FB_CONFIG.FB_VERSION}/access_token?`;
+  const fbVerifyUrl = `https://graph.accountkit.com/${FB_CONFIG.FB_VERSION}/me?`;
   const query = {
     grant_type: 'authorization_code',
     code: req.body.code,
-    access_token: ['AA', process.env.FB_APP_ID, process.env.FB_APP_SECRET].join('|'),
+    access_token: ['AA', FB_CONFIG.FB_APP_ID, FB_CONFIG.FB_APP_SECRET].join('|'),
   };
 
   try {
@@ -35,7 +41,7 @@ const fbIssueToken = async (req: Request, res: Response, next: NextFunction) => 
       };
     }> = await axios.get(`${fbVerifyUrl}access_token=${fbIssueResponse.data.access_token}`);
 
-    if (fbVerifyResponse.data.application.id === process.env.FB_APP_SECRET) {
+    if (fbVerifyResponse.data.application.id === FB_CONFIG.FB_APP_SECRET) {
       res.locals.temp = {
         ...res.locals.temp,
         tp: '0' + fbVerifyResponse.data.phone.national_number,
