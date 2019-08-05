85b75e4e66b872d68adcd9199fea681affc88509
Nods <taxci79@gmail.com>
2019-08-06 02:52:16 +0900

    feat: POST /user/image

--- routes/middlewares/user/get/getUser.ts
+++ routes/middlewares/user/get/getUser.ts
@@ -17,6 +17,7 @@ const getUser = (req: Request, res: Response, next: NextFunction) => {
         grade: user.type === 'student' ? user.student.grade : null,
         classNum: user.type === 'student' ? user.student.classNum : null,
         studentNum: user.type === 'student' ? user.student.studentNum : null,
+        image: user.image ? `https://s3.ap-northeast-2.amazonaws.com/hanlight/profile-image/${user.image}` : null,
       },
     },
   });
