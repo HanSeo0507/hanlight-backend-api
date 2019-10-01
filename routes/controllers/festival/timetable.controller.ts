20dc97a8e09806bf4e7d7f20b0c9360352191c57
kb4ek <kbaek1202@gmail.com>
2019-10-01 12:57:49 +0900

    change lowercase to uppercase 'FS'

--- routes/controllers/festival/timetable.controller.ts
+++ routes/controllers/festival/timetable.controller.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import getFSTimeTable from '@Middleware/festival/timetable/getfsTimeTable';
+import getFSTimeTable from '@Middleware/festival/timetable/getFSTimeTable';
 
 const router: Router = Router();
 
